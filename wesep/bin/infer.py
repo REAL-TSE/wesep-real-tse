0eff87243216230ed206fce709f9be88411d70a3
Ke Zhang <kylezhang1118@gmail.com>
2026-03-25 13:25:18 +0000

    Fix bug in infer.py

--- wesep/bin/infer.py
+++ wesep/bin/infer.py
@@ -18,6 +18,7 @@ from wesep.dataset.collate import (
 from wesep.models import get_model
 from wesep.utils.checkpoint import load_pretrained_model
 from wesep.utils.score import cal_SISNRi
+from wesep.utils.file_utils import load_yaml
 from wesep.utils.utils import (
     generate_enahnced_scp,
     get_logger,
@@ -77,12 +78,12 @@ def infer(config="confs/conf.yaml", **kwargs):
     model = model.to(device)
     model.eval()
 
+    configs["dataset_args"]["whole_utt"] = True
     test_dataset = Dataset(
         configs["data_type"],
         configs["test_data"],
         configs["dataset_args"],
         state="test",
-        whole_utt=configs.get("whole_utt", True),
         repeat_dataset=configs.get("repeat_dataset", False),
         cues_yaml=configs.get("test_cues", None),
     )
@@ -96,7 +97,7 @@ def infer(config="confs/conf.yaml", **kwargs):
         batch_size=1,
         collate_fn=lambda batch: tse_collate_fn(batch, test_collect_keys))
 
-    with open(configs["val_samples"], "r", encoding="utf-8") as f:
+    with open(configs["test_data"], "r", encoding="utf-8") as f:
         test_iter = sum(1 for _ in f)
     logger.info("test number: {}".format(test_iter))
 
@@ -138,14 +139,12 @@ def infer(config="confs/conf.yaml", **kwargs):
             ests = outputs
             mix = mix.cpu().numpy()
 
-            if ests[0].size != ref[0].size:
-                end = min(ests[0].size, ref[0].size, mix[0].size)
-                ests_1 = ests[0][:end]
-                ref_1 = ref[0][:end]
-                mix_1 = mix[0][:end]
-                SISNR1, delta1 = cal_SISNRi(ests_1, ref_1, mix_1)
-            else:
-                SISNR1, delta1 = cal_SISNRi(ests[0], ref[0], mix[0])
+            min_len = min(ref.shape[-1], ests.shape[-1], mix.shape[-1])
+            ref = ref[..., :min_len]
+            ests = ests[..., :min_len]
+            mix = mix[..., :min_len]
+
+            SISNR1, delta1 = cal_SISNRi(ests[0], ref[0], mix[0])
 
             logger.info(
                 "Num={} | Utt={} | Target speaker={} | SI-SNR={:.2f} | SI-SNRi={:.2f}"
@@ -156,14 +155,7 @@ def infer(config="confs/conf.yaml", **kwargs):
             if delta1 > 1:
                 accept_cnt += 1
 
-            if ests[1].size != ref[1].size:
-                end = min(ests[1].size, ref[1].size, mix[1].size)
-                ests_2 = ests[1][:end]
-                ref_2 = ref[1][:end]
-                mix_2 = mix[1][:end]
-                SISNR2, delta2 = cal_SISNRi(ests_2, ref_2, mix_2)
-            else:
-                SISNR2, delta2 = cal_SISNRi(ests[1], ref[1], mix[1])
+            SISNR2, delta2 = cal_SISNRi(ests[1], ref[1], mix[1])
             logger.info(
                 "Num={} | Utt={} | Target speaker={} | SI-SNR={:.2f} | SI-SNRi={:.2f}"
                 .format(total_cnt + 1, key[1], spk[1], SISNR2, delta2))
@@ -173,8 +165,6 @@ def infer(config="confs/conf.yaml", **kwargs):
             if delta2 > 1:
                 accept_cnt += 1
 
-            # if (i + 1) == test_iter:
-            #     break
         end = time.time()
     # generate the scp file of the enhanced speech for scoring
     if sign_save_wav:
